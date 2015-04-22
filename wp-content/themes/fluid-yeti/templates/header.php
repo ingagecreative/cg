56b063a3985f60c9a1a304bda67561e201277cdc
Matt Dignan <matt.dignan@ingagecreative.com>
2015-04-21 23:34:10 -0600

    updates

--- wp-content/themes/fluid-yeti/templates/header.php
+++ wp-content/themes/fluid-yeti/templates/header.php
@@ -12,19 +12,21 @@
         <?php if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'right'));
         endif; ?>
-        <!-- <ul class="social-header">
-          <?php //if(have_rows('social_media', 'option')) : ?>
-            <?php //while(have_rows('social_media', 'option')) : the_row(); ?>
-              <li><a href="<?php //the_sub_field('social_link') ?>"><?php //the_sub_field('social_icon') ?></a></li>
-            <?php //endwhile; ?>
-          <?php //else: endif; ?>
-        </ul> -->
       </section>
     </nav>
   </header>
   <section class="main-sub-nav">
     <div class="row medium-collapse">
-      <div class="medium-12 columns">
+      <div class="medium-6 columns">
+        <ul class="social-header">
+          <?php if(have_rows('social_media', 'option')) : ?>
+            <?php while(have_rows('social_media', 'option')) : the_row(); ?>
+              <li><a href="<?php the_sub_field('social_link') ?>"><?php the_sub_field('social_icon') ?></a></li>
+            <?php endwhile; ?>
+          <?php else: endif; ?>
+        </ul>
+      </div>
+      <div class="medium-6 columns">
         <?php echo do_shortcode("[constantcontactapi formid='1']"); ?>  
       </div>
     </div>      
