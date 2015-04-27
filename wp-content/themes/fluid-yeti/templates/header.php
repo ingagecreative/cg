1badc7e01db1d88bf6825fed126e94252999982d
Matt Dignan <matt.dignan@ingagecreative.com>
2015-04-27 14:40:34 -0600

    updates

--- wp-content/themes/fluid-yeti/templates/header.php
+++ wp-content/themes/fluid-yeti/templates/header.php
@@ -7,17 +7,7 @@
         <li class="name"> <h1><a href="<?php echo esc_url(home_url()); ?>"><img src="<?php echo esc_url(home_url()); ?>/wp-content/themes/fluid-yeti/assets/img/coz.jpg"></a></h1> </li>
         <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
       </ul>
-
-      <section class="top-bar-section">
-        <?php if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'right'));
-        endif; ?>
-      </section>
-    </nav>
-  </header>
-  <section class="main-sub-nav">
-    <div class="row medium-collapse">
-      <div class="medium-6 columns">
+      <div class="right">
         <ul class="social-header">
           <?php if(have_rows('social_media', 'option')) : ?>
             <?php while(have_rows('social_media', 'option')) : the_row(); ?>
@@ -26,7 +16,16 @@
           <?php else: endif; ?>
         </ul>
       </div>
-      <div class="medium-6 columns">
+      <section class="top-bar-section">
+        <?php if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'left'));
+        endif; ?>
+      </section>
+    </nav>
+  </header>
+  <section class="main-sub-nav">
+    <div class="row medium-collapse">
+      <div class="medium-12 text-right columns">
         <?php echo do_shortcode("[constantcontactapi formid='1']"); ?>  
       </div>
     </div>      
